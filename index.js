b7164b6fce134a8e626cf2122dc2f51b65cedd2d
LtGriff <ltgriff96@gmail.com>
2022-04-25 12:04:31 -0400

    push

---
+++ index.js
@@ -0,0 +1,30 @@
+function testGreaterThan(val) {
+    if (val > 150) {  // Change this line
+      return "Over 150";
+    }
+  
+    if (val > 20) {  // Change this line
+      return "Over 20";
+    }
+  
+    return "20 or Under";
+  }
+  
+  testGreaterThan(20);
+
+
+
+
+  function testGreaterThan(val) {
+    if (val > 160) {  // Change this line
+      return "Over 160";
+    }
+  
+    if (val > 40) {  // Change this line
+      return "Over 40";
+    }
+  
+    return "30 or Under";
+  }
+  
+  testGreaterThan(40);
